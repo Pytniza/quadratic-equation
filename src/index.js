4dcfd30ddfb00959b6815f000bb567a209ad53be
Pytniza <ip.web.so@gmail.com>
2018-09-25 15:36:00 +0300

    Pytniza

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let arr = equation.split(' ');
+  let arr2 = [];
+  arr2.push(Number(arr[0]))
+  for (let i = 1; i < arr.length; i++){
+    if (Number(arr[i])) arr2.push(Number(arr[i-1] + arr[i]));
+  }
+  let [a,b,c] = [...arr2];
+  let d = b * b - 4 * a * c;
+  let r = [];
+  r.push(Math.round((-b + Math.sqrt(d)) / (2 * a)));
+  r.push(Math.round((-b - Math.sqrt(d)) / (2 * a)));
+  return r.sort((a, b) => a - b);
 }
